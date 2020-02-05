d17d1c56169366580749377d8f07e6c2ca1074b4
cullenMichael <cullenm9@tcd.ie>
2020-02-05 23:33:42 +0000

    Added Signin page with error signals for invalid input.

---
+++ frontend_app/app/src/main/java/com/example/trocaire_disease_outbreak_manager/Patient_details.java
@@ -0,0 +1,19 @@
+package com.example.trocaire_disease_outbreak_manager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Patient_details extends Activity implements View.OnClickListener {
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patient_details);
+        }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
