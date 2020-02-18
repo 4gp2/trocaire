3ab1b6584d97eb07121967f70105049e8f6ded69
cullenMichael <cullenm9@tcd.ie>
2020-02-18 17:49:56 +0000

    Moved GPS locator from Signin to patient review page

--- frontend_app/app/src/main/java/com/example/trocaire_disease_outbreak_manager/Review_patient_details.java
+++ frontend_app/app/src/main/java/com/example/trocaire_disease_outbreak_manager/Review_patient_details.java
@@ -1,18 +1,34 @@
 package com.example.trocaire_disease_outbreak_manager;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Review_patient_details extends AppCompatActivity implements View.OnClickListener {
+import java.util.List;
 
+public class Review_patient_details extends AppCompatActivity implements View.OnClickListener, LocationListener {
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +43,21 @@ public class Review_patient_details extends AppCompatActivity implements View.On
 
         Button next = findViewById(R.id.buttonreviewdetails);
         next.setOnClickListener(this);
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+        }else{
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            Location locationCurrent = getLastKnownLocation();
+            double latitude = locationCurrent.getLatitude();
+            double longitude = locationCurrent.getLongitude();
+            String res = latitude + " " + longitude;
+            Toast.makeText(this, res,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -35,9 +66,69 @@ public class Review_patient_details extends AppCompatActivity implements View.On
         Review_patient_details.this.startActivity(con);
     }
 
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("GPS is Required for this app, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private Location getLastKnownLocation() {
+        LocationManager mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            }
+            Location loc = mLocationManager.getLastKnownLocation(provider);
+            if (loc == null) {
+                continue;
+            }
+            if (bestLocation == null || loc.getAccuracy() < bestLocation.getAccuracy()) {
+                bestLocation = loc;
+            }
+        }
+        return bestLocation;
+    }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
