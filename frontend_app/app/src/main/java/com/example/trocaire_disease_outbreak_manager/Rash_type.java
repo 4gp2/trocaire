b6a0c5dce8507b80182f96ecb745a25f61cd75b6
cullenMichael <cullenm9@tcd.ie>
2020-02-27 11:36:33 +0000

    Created UI for rash Identification with checkboxes

--- frontend_app/app/src/main/java/com/example/trocaire_disease_outbreak_manager/Rash_type.java
+++ frontend_app/app/src/main/java/com/example/trocaire_disease_outbreak_manager/Rash_type.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,12 +26,27 @@ public class Rash_type extends AppCompatActivity implements View.OnClickListener
         bar.setDisplayHomeAsUpEnabled(true);
         bar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
 
-        Button next = findViewById(R.id.buttonrashtype);
+        Button next = findViewById(R.id.btnSubmitRashType);
         next.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+
+        CheckBox clustered =  findViewById(R.id.chkClustered);
+        CheckBox large_patches =  findViewById(R.id.chkLargePatches);
+        CheckBox mixture =  findViewById(R.id.chkMixture);
+        CheckBox single_scaley = findViewById(R.id.chkSingleScaley);
+        CheckBox small_spots =  findViewById(R.id.chkSmallSpots);
+        CheckBox small_scaley = findViewById(R.id.SmallScaley);
+
+        Boolean clustered_isChecked = clustered.isChecked();
+        Boolean large_patches_isChecked = large_patches.isChecked();
+        Boolean mixture_isChecked = mixture.isChecked();
+        Boolean single_scaley_isChecked = single_scaley.isChecked();
+        Boolean small_spots_isChecked = small_spots.isChecked();
+        Boolean small_scaley_isChecked = small_scaley.isChecked();
+
         Intent con = new Intent(Rash_type.this, Rash_locator_front.class);
         Rash_type.this.startActivity(con);
     }
