16a3b9bb26f9c1ccb9afef3f78df6f7253777e7e
ZINAHAL <albaghdz@tcd.ie>
2020-03-04 22:52:15 +0000

    Added code that will allow the admin to view the patients details

--- backend/static/js/dashboard/main.js
+++ backend/static/js/dashboard/main.js
@@ -1,3 +1,8 @@
+const patient_list_container = document.getElementById('plist');
+const patient_template = document.querySelector('template').content.cloneNode(true);
+const patient_details_container = document.getElementById('pdetails');
+var database_response = null;
+
 const initFirebase = () => {
   firebase.initializeApp({
     apiKey: 'AIzaSyCizCQ2QIyUZ6ONcHwmyGmHPyB6jwQji2c',
@@ -23,6 +28,43 @@ const bootstrapElements = () => {
     window.location.href = window.location.origin;
   });
 };
+
+document.getElementById('searchB').addEventListener('click', async e => {
+  e.preventDefault();
+  patient_list_container.innerHTML = "";
+
+  const token = await firebase.auth().currentUser.getIdToken(true);
+  database_response = await axios.post('/api/patient', {
+    token,
+    lastName: document.getElementById('lName').value,
+    firstName: document.getElementById('fName').value,
+    year: document.getElementById('year').value.toString(),
+    month: document.getElementById('month').value.toString(),
+    day: document.getElementById('day').value.toString(),
+  });
+
+  if(database_response.data.error) {
+    patient_list_container.appendChild(patient_template.getElementById('noPatientFound'));
+    console.log("no search found");
+    return;
+  }
+
+  patient_template.getElementById('pNameFound').textContent = database_response.data.firstName + " " + database_response.data.lastName;
+  patient_list_container.appendChild(patient_template.getElementById('pNameFound'));
+});
+
+patient_template.getElementById('pNameFound').addEventListener('click', async e => {
+  e.preventDefault();
+  patient_details_container.appendChild(document.createElement('form'));
+  database_response.data.forEach(element => {
+    patient_template.getElementById('pContentRow').querySelector('label').textContent = element.key;
+    patient_template.getElementById('pContentRow').querySelector('input').setAttribute('value', element.value);
+    patient_details_container.querySelector('form').appendChild(patient_template.getElementById('pContentRow'));
+  });
+});
+
+// console.log(axios.get("https://jsonplaceholder.typicode.com/users"));
+
 
 initFirebase();
 bootstrapElements();
