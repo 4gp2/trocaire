dec99b2508c0103f5f1d10ed1ce1326cbdb6ad00
David Scolard <dscolard@tcd.ie>
2020-03-24 18:38:58 +0000

    Display checked symptoms for patients when updating

--- frontend_app/app/src/main/java/com/example/trocaire_disease_outbreak_manager/Symptoms_input.java
+++ frontend_app/app/src/main/java/com/example/trocaire_disease_outbreak_manager/Symptoms_input.java
@@ -14,11 +14,21 @@ import android.widget.CheckBox;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Symptoms_input extends AppCompatActivity implements View.OnClickListener {
+    private static final String FILE_NAME = "patients.json";
+
+    public static Object DOB, firstName, lastName;
 
     private CheckBox chkNausea, chkDiahorrea, chkHeartRate, chkMusclePain, chkDehydration,
             chkCough, chkSoreThroat, chkHeadache, chkRash, chkSweating, chkBloodyStools,
@@ -33,6 +43,88 @@ public class Symptoms_input extends AppCompatActivity implements View.OnClickLis
         try {
             data = new JSONObject(getIntent().getStringExtra("data"));
         } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            firstName = data.get("firstName");
+            lastName = data.get("lastName");
+            DOB =  data.get("dob");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //check if these match any
+
+
+        try {
+            String json_String = get_json_string();
+            JSONObject obj = new JSONObject(json_String);
+            JSONArray userArray = obj.getJSONArray("patients");
+            for (int i = 0; i < userArray.length(); i++) {
+                JSONObject userDetail = userArray.getJSONObject(i);
+
+                //If this patient exists already then show symptoms
+                if (userDetail.get("firstName").equals(firstName) && userDetail.get("lastName").equals(lastName)
+                        && userDetail.get("dob").equals(DOB)){
+
+                    if(userDetail.getJSONObject("symptoms").getBoolean("nausea")){
+                        CheckBox nausea = this.findViewById(R.id.chkNausea);
+                        nausea.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("diarrhea")){
+                        CheckBox diarrhea = this.findViewById(R.id.chkDiahorrea);
+                        diarrhea.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("highHeartRate")){
+                        CheckBox heartRate = this.findViewById(R.id.chkHeartRate);
+                        heartRate.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("dehydration")){
+                        CheckBox dehydration = this.findViewById(R.id.chkDehydration);
+                        dehydration.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("muscleAches")){
+                        CheckBox muscleAche = this.findViewById(R.id.chkMusclePain);
+                        muscleAche.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("dryCough")){
+                        CheckBox dryCough = this.findViewById(R.id.chkCough);
+                        dryCough.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("soreThroat")){
+                        CheckBox soreThroat = this.findViewById(R.id.chkSoreThroat);
+                        soreThroat.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("headache")){
+                        CheckBox headache = this.findViewById(R.id.chkHeadache);
+                        headache.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("sweating")){
+                        CheckBox sweating = this.findViewById(R.id.chkSweating);
+                        sweating.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("bloodyStool")){
+                        CheckBox bloodyStool = this.findViewById(R.id.chkBloodyStools);
+                        bloodyStool.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("mouthSpots")){
+                        CheckBox mouthSpot = this.findViewById(R.id.chkMouthSpots);
+                        mouthSpot.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getBoolean("stiffLimbs")){
+                        CheckBox stiffLimb = this.findViewById(R.id.chkStiffLimbs);
+                        stiffLimb.setChecked(true);
+                    }
+                    if(userDetail.getJSONObject("symptoms").getJSONObject("rash").getBoolean("hasRash")){
+                        CheckBox rash = this.findViewById(R.id.chkRash);
+                        rash.setChecked(true);
+                    }
+
+                }
+
+            }
+        } catch (JSONException | IOException e) {
             e.printStackTrace();
         }
 
@@ -98,4 +190,28 @@ public class Symptoms_input extends AppCompatActivity implements View.OnClickLis
         onBackPressed();
         return true;
     }
+
+    private String get_json_string() throws IOException {
+
+        File file = new File (this.getFilesDir(), FILE_NAME);
+        if (file.exists()){
+            StringBuilder output = new StringBuilder();
+            FileReader fileReader;
+            try {
+                fileReader = new FileReader(file.getAbsoluteFile());
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+                    output.append(line).append("\n");
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return output.toString();
+        }
+        return "";
+    }
+
 }
